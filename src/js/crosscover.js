7796dfd936ae34804b61da238df1e97b96ab3ed6
blivesta <enmt@blivesta.com>
2015-08-07 03:01:13 +0900

    refactor and add some options

--- src/js/crosscover.js
+++ src/js/crosscover.js
@@ -1,4 +1,4 @@
-;(function (factory) {
+;(function(factory) {
   'use strict';
   if (typeof define === 'function' && define.amd) {
     define(['jquery'], factory);
@@ -7,42 +7,38 @@
   } else {
     factory(jQuery);
   }
-}(function ($) {
+}(function($) {
   'use strict';
   var namespace = 'crosscover';
-  var autoPlay = null;
-	var timer = null;
-	var current = null;
-  var methods = {
+  var __ = {
     init: function(options) {
       options = $.extend({
-        animateInClass:'fade-in',
-        animateOutClass:'fade-out',
-				interval : 5000,
-				startIndex : 0,
-        autoPlay : true,
-				controles : true,
-				controlesClass  : 'crosscover-controls',
-				prevClass : 'crosscover-prev',
-				nextClass : 'crosscover-next',
-				playerClass : 'crosscover-player',
-				playerActiveClass : 'is-playing'
+        animateInClass: 'fade-in',
+        animateOutClass: 'fade-out',
+        interval: 5000,
+        startIndex: 0,
+        autoPlay: true,
+        controller: true,
+        controllerClass: 'crosscover-controller',
+        prevClass: 'crosscover-prev',
+        nextClass: 'crosscover-next',
+        playerClass: 'crosscover-player',
+        playerActiveClass: 'is-playing',
+        playerInnerHtml: '<span class="crosscover-icon-player"></span>',
+        prevInnerHtml: '<span class="crosscover-icon-prev"></span>',
+        nextInnerHtml: '<span class="crosscover-icon-next"></span>'
       }, options);
 
-			autoPlay = options.autoPlay;
-      if (options.controles) methods.addControler.call(this,options);
+      __.settings = {
+        currentIndex: options.startIndex,
+        timer: null
+      };
 
       return this.each(function() {
         var _this = this;
         var $this = $(this);
         var data = $this.data(namespace);
-				var $items = $this.children('.crosscover-list').children('li');
-				var $controls = $this.children('.' + options.controlesClass);
-				var	$navPrev = $controls.children('.' + options.prevClass);
-				var	$navNext = $controls.children('.' + options.nextClass);
-				var	$navPlayPause = $controls.children('.' + options.playerClass);
-
-				current = options.startIndex;
+        var $item = $this.children('.crosscover-list').children('li');
 
         if (!data) {
           options = $.extend({}, options);
@@ -50,157 +46,221 @@
             options: options
           });
 
-          methods.setup.call(_this, $items);
+          if (options.controller) __.createControler.call(_this, $item);
 
-					$navPlayPause.on('click.' + namespace, function(event) {
-  					 methods.pauseToggle.call(_this, $items, event);
-					});
-
-					$navPrev.on('click.' + namespace, function() {
-						if(autoPlay) methods.autoPlayStart.call(_this, $items);
-						return methods.toggle.call(_this, $items, 'prev');
-					} );
-
-					$navNext.on('click.' + namespace, function() {
-						if(autoPlay) methods.autoPlayStart.call(_this, $items);
-						return methods.toggle.call(_this, $items, 'next');
-					});
+          __.setup.call(_this, $item);
 
         }
       });
     },
 
-    addControler: function(options) {
+    setup: function($item) {
+      var _this = this;
       var $this = $(this);
-      var isClass = autoPlay ? options.playerActiveClass : null;
+      var options = $this.data(namespace).options;
+
+      $item.each(function(index) {
+        var $self = $(this);
+        var image = $self.find('img').attr('src');
+        $self
+          .addClass('crosscover-setup')
+          .css({
+            'background-image': 'url(' + image + ')'
+          });
+      });
+
+      return __.slideStart.call(_this, $item);
+
+    },
+
+    slideStart: function($item) {
+      var _this = this;
+
+      __.autoPlayStart.call(_this, $item);
+      __.show.call(_this, $item);
+    },
+
+    createControler: function($item) {
+      var _this = this;
+      var $this = $(this);
+      var options = $this.data(namespace).options;
+      var isClass = options.autoPlay ? options.playerActiveClass : null;
+
       $this
-	      .append(
+        .append(
           $('<div>')
-            .addClass(options.controlesClass)
-    	      .append(
-              $('<span>')
-                .addClass(options.prevClass)
-            )
-    	      .append(
-              $('<span>')
-                .addClass(options.playerClass)
-                .addClass(isClass)
-            )
-    	      .append(
-              $('<span>')
-                .addClass(options.nextClass)
-            )
+          .attr({
+            'data-crosscover-controls': ''
+          })
+          .addClass(options.controllerClass)
+          .append(
+            $('<button>')
+            .attr({
+              'data-crosscover-prev': ''
+            })
+            .addClass(options.prevClass)
+            .html(options.prevInnerHtml)
+          )
+          .append(
+            $('<button>')
+            .attr({
+              'data-crosscover-player': ''
+            })
+            .addClass(options.playerClass)
+            .addClass(isClass)
+            .html(options.playerInnerHtml)
+          )
+          .append(
+            $('<button>')
+            .attr({
+              'data-crosscover-next': ''
+            })
+            .addClass(options.nextClass)
+            .html(options.nextInnerHtml)
+          )
         );
+
+      return __.addControler.call(_this, $item);
     },
 
-    setup: function($items) {
+    addControler: function($item) {
       var _this = this;
       var $this = $(this);
       var options = $this.data(namespace).options;
-      $items.each(function(index) {
-				var $item = $(this);
-				$item
-          .css( 'background-image', 'url(' + $item.find( 'img' )
-          .attr( 'src' ) + ')' );
-			});
-			$items
-        .eq(current)
-        .addClass('crosscover-active')
-        .addClass(options.animateInClass);
-      return methods.autoPlayStart.call(_this, $items);
+      var $controls = $this.children('[data-crosscover-controls]');
+      var $navPrev = $controls.children('[data-crosscover-prev]');
+      var $navNext = $controls.children('[data-crosscover-next]');
+      var $navPlayPause = $controls.children('[data-crosscover-player]');
+
+      $navPlayPause.on('click.' + namespace, function(event) {
+        $(this).blur();
+        return __.playToggle.call(_this, $item, $(this));
+      });
+
+      $navPrev.on('click.' + namespace, function(event) {
+        $(this).blur();
+        return __.toggle.call(_this, $item, 'prev');
+      });
+
+      $navNext.on('click.' + namespace, function(event) {
+        $(this).blur();
+        return __.toggle.call(_this, $item, 'next');
+      });
+
     },
 
-    pauseToggle: function($items, event) {
+    playToggle: function($item) {
       var _this = this;
       var $this = $(this);
       var options = $this.data(namespace).options;
-  		var $target = $(event.target);
+      var $navPlayPause = $this.find('[data-crosscover-player]');
 
-      if(autoPlay){
-        autoPlay = false;
-  			$target
-  				.removeClass(options.playerActiveClass)
-  				.addClass(options.playClass);
-        return methods.autoPlayStop.call(_this);
+      if (options.autoPlay) {
+
+        options.autoPlay = false;
+
+        $navPlayPause
+          .removeClass(options.playerActiveClass)
+          .addClass(options.playClass);
+
+        return __.autoPlayStop.call(_this);
       } else {
-        autoPlay = true;
-  			$target.addClass(options.playerActiveClass);
-        return methods.autoPlayStart.call(_this, $items);
+
+        options.autoPlay = true;
+
+        $navPlayPause.addClass(options.playerActiveClass);
+
+        return __.autoPlayStart.call(_this, $item);
       }
     },
 
-    toggle: function($items, mode){
-      var _this = this;
-			var $currentItem = $items.eq(current);
-			if(mode === 'next') {
-				 current = current < $items.length - 1 ? current++ : 0 - 1;
-         current++;
-			} else if(mode === 'prev'){
-				 current = current > 0 - 1 ? current-- : $items.length - 1;
-         current--;
-			}
-      var $newItem = $items.eq(current);
-      methods.animate.call(_this, $currentItem, $newItem);
-    },
-
-    animate: function($currentItem, $newItem) {
+    toggle: function($item, setting) {
       var _this = this;
       var $this = $(this);
       var options = $this.data(namespace).options;
-			$currentItem
-        .removeClass()
-        .addClass(options.animateOutClass)
-        .animateCallback(function(){
-          $(this).removeClass();
-        });
-			$newItem
-        .addClass(options.animateInClass)
+
+      __.hide.call(_this, $item);
+
+      if (setting === 'next') {
+        __.settings.currentIndex++;
+      } else if (setting === 'prev') {
+        __.settings.currentIndex--;
+      }
+
+      if (__.settings.currentIndex >= $item.length) {
+        __.settings.currentIndex = 0;
+      } else if (__.settings.currentIndex <= -1) {
+        __.settings.currentIndex = $item.length - 1;
+      }
+
+      __.autoPlayStart.call(_this, $item);
+
+      return __.show.call(_this, $item);
+    },
+
+    show: function($item) {
+      var $this = $(this);
+      var options = $this.data(namespace).options;
+
+      return $item
+        .eq(__.settings.currentIndex)
         .addClass('crosscover-active')
-        .animateCallback(function(){
-          if(!$(this).hasClass('crosscover-active')){
-            $(this).addClass('crosscover-active');
-          }
+        .removeClass('crosscover-setup')
+        .addClass(options.animateInClass)
+        .animateEnd(function() {
+          $(this).addClass('crosscover-active');
         });
     },
 
-    currentIndex: function() {
-      return current;
+    hide: function($item) {
+      var $this = $(this);
+      var options = $this.data(namespace).options;
+
+      return $item
+        .eq(__.settings.currentIndex)
+        .addClass('crosscover-setup')
+        .removeClass('crosscover-active')
+        .addClass(options.animateOutClass)
+        .animateEnd(function() {
+          $(this)
+            .removeClass()
+            .addClass('crosscover-setup');
+        });
     },
 
-    autoPlayStart: function($items) {
+    autoPlayStart: function($item) {
       var _this = this;
       var $this = $(this);
-      var options   = $this.data(namespace).options;
-      if(autoPlay){
-        clearTimeout( timer );
-  			timer = setTimeout(function(){
-  				methods.toggle.call(_this, $items, 'next' );
-  				methods.autoPlayStart.call(_this, $items);
-  			}, options.interval );
+      var options = $this.data(namespace).options;
+
+      if (options.autoPlay) {
+
+        __.autoPlayStop.call(_this);
+
+        __.settings.timer = setTimeout(function() {
+          __.toggle.call(_this, $item, 'next');
+          __.autoPlayStart.call(_this, $item);
+        }, options.interval);
+
       }
+      return _this;
     },
 
     autoPlayStop: function() {
-  		autoPlay = false;
-  		return clearTimeout( timer );
+      return clearTimeout(__.settings.timer);
     },
 
-    destroy: function(){
-      return this.each(function(){
-        var $this = $(this);
-        $(window).unbind('.'+namespace);
-        $this
-          .css({'opacity':1})
-          .removeData(namespace);
-      });
+    currentIndex: function() {
+      return __.settings.currentIndex;
     }
+
   };
 
-  $.fn.animateCallback = function(callback){
+  $.fn.animateEnd = function(callback) {
     var end = 'animationend webkitAnimationEnd mozAnimationEnd oAnimationEnd MSAnimationEnd';
     return this.each(function() {
       var $this = $(this);
-      $this.bind(end, function(){
+      $this.bind(end, function() {
         $this.unbind(end);
         return callback.call(this);
       });
@@ -208,10 +268,10 @@
   };
 
   $.fn.crosscover = function(method) {
-    if (methods[method]) {
-      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    if (__[method]) {
+      return __[method].apply(this, Array.prototype.slice.call(arguments, 1));
     } else if (typeof method === 'object' || !method) {
-      return methods.init.apply(this, arguments);
+      return __.init.apply(this, arguments);
     } else {
       $.error('Method ' + method + ' does not exist on jQuery.' + namespace);
     }
